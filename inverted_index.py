b40c0701ccfb45e53c00cf6cabe220eea4e54df5
Alexandros Xiarchos <Alex.xiarxos@gmail.com>
2024-01-05 06:10:23 +0200

    queries

--- inverted_index.py
+++ inverted_index.py
@@ -1,19 +1,4 @@
-import re
-
-# Αφαίρεση των \n χαρακτήρων από τα docs χρησιμοποιώντας regex
-def remove_pattern_from_string(text, pattern):
-    replacement = ""
-    modified_text = re.sub(pattern, replacement, text)
-    return modified_text
-
-
-# Εισάγεται ένας αριθμός (1-1239) και επιστρέφεται το path του αρχείου με αυτό το νούμερο
-# Δεν υπάρχει ανάγκη για έλεγχο του range του αριθμού, μιας και ελέγχονται τα exceptions
-def get_doc_path(number):
-    file_path_prefix = "/Users/alex/Library/Mobile Documents/com~apple~CloudDocs/Documents/CEID/ΕΠΙΛΟΓΕΣ ΧΕΙΜΕΡΙΝΟΥ/ΑΝΑΚΤΗΣΗ ΠΛΗΡΟΦΟΡΙΑΣ/PROJECT/Collection/docs/"
-    file_path_suffix = str(number).zfill(5)  # 1 -> 00001
-    file_path = file_path_prefix + file_path_suffix
-    return file_path
+import tools
 
 
 def create_inverted_index():
@@ -22,7 +7,7 @@ def create_inverted_index():
     word_dict = {}
 
     for doc_number in range(1, 1240):  # 1239 έγγραφα
-        doc = get_doc(doc_number)
+        doc = tools.get_doc(doc_number)
         try:
             for doc_content in doc:  # Το doc βρίσκεται σε μορφή list, το doc_content το χωρίζει σε λέξεις.
                 for doc_word in doc_content.split():
@@ -36,7 +21,7 @@ def create_inverted_index():
         word_dict[i] = dict.fromkeys(total_words, 0)
 
     for doc_number in range(1, 1240):  # 1239 έγγραφα
-        doc = get_doc(doc_number)
+        doc = tools.get_doc(doc_number)
 
         try:
             for doc_content in doc:  # Το doc βρίσκεται σε μορφή list, το doc_content το χωρίζει σε λέξεις.
@@ -56,21 +41,3 @@ def create_inverted_index():
     print("Το ανεστραμμένο ευρετήριο δημιουργήθηκε.")
 
     return sorted_inverted_index, word_dict
-
-
-# Δίνεις 5 και επιστρέφεται μια λίστα με το περιεχόμενο του εγγράφου 00005
-def get_doc(doc_number):
-    doc_path = get_doc_path(doc_number)  # Ανοίγεται το κάθε doc ξεχωριστά
-
-    try:
-        with open(doc_path, "r") as doc_file:
-            doc = doc_file.readlines()  # ανάγνωση document
-
-        # Αφαίρεση "\n" χαρακτήρων από το περιεχόμενο του document
-        for index, word in enumerate(doc):
-            doc[index] = remove_pattern_from_string(doc[index], r"\n")
-
-        return doc
-
-    except FileNotFoundError:
-        pass
---
+++ tools.py
@@ -0,0 +1,47 @@
+import re
+
+
+# Αφαίρεση των \n χαρακτήρων από τα docs χρησιμοποιώντας regex
+def remove_pattern_from_string(text, pattern):
+    replacement = ""
+    modified_text = re.sub(pattern, replacement, text)
+    return modified_text
+
+
+# Εισάγεται ένας αριθμός (1-1239) και επιστρέφεται το path του αρχείου με αυτό το νούμερο
+# Δεν υπάρχει ανάγκη για έλεγχο του range του αριθμού, μιας και ελέγχονται τα exceptions
+def get_doc_path(number):
+    file_path_prefix = "/Users/alex/Library/Mobile Documents/com~apple~CloudDocs/Documents/CEID/ΕΠΙΛΟΓΕΣ ΧΕΙΜΕΡΙΝΟΥ/ΑΝΑΚΤΗΣΗ ΠΛΗΡΟΦΟΡΙΑΣ/PROJECT/Collection/docs/"
+    file_path_suffix = str(number).zfill(5)  # 1 -> 00001
+    file_path = file_path_prefix + file_path_suffix
+    return file_path
+
+
+# Δίνεις 5 και επιστρέφεται μια λίστα με το περιεχόμενο του εγγράφου 00005
+def get_doc(doc_number):
+    doc_path = get_doc_path(doc_number)  # Ανοίγεται το κάθε doc ξεχωριστά
+
+    try:
+        with open(doc_path, "r") as doc_file:
+            doc = doc_file.readlines()  # ανάγνωση document
+
+        # Αφαίρεση "\n" χαρακτήρων από το περιεχόμενο του document
+        for index, word in enumerate(doc):
+            doc[index] = remove_pattern_from_string(doc[index], r"\n")
+
+        return doc
+
+    except FileNotFoundError:
+        pass
+
+
+def get_queries():
+    file_path = "/Users/alex/Library/Mobile Documents/com~apple~CloudDocs/Documents/CEID/ΕΠΙΛΟΓΕΣ ΧΕΙΜΕΡΙΝΟΥ/ΑΝΑΚΤΗΣΗ ΠΛΗΡΟΦΟΡΙΑΣ/PROJECT/Collection/Queries_20"
+    with open(file_path, 'r') as queries_file:
+        queries = queries_file.readlines()
+
+    # Αφαίρεση "\n" χαρακτήρων από το περιεχόμενο του document
+    for index, query in enumerate(queries):
+        queries[index] = remove_pattern_from_string(queries[index], r"\n")
+
+    return queries
