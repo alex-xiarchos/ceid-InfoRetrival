9b34738b2cef89149cc89854acc50d58c5658cdf
Alexandros Xiarchos <Alex.xiarxos@gmail.com>
2024-01-17 19:01:58 +0200

    update

--- inverted_index.py
+++ inverted_index.py
@@ -4,9 +4,9 @@ import tools
 def create_inverted_index():
     inverted_index = {}
     total_words = set()
-    word_dict = {}
 
-    for doc_number in range(1, 1240):  # 1239 έγγραφα
+    # Δημουργείται ένα set (total_words) με το σύνολο όλων των λέξεων που υπάρχουν στα έγγραφα:
+    for doc_number in range(1, 100):  # 1239 έγγραφα
         doc = tools.get_doc(doc_number)
         try:
             for doc_content in doc:  # Το doc βρίσκεται σε μορφή list, το doc_content το χωρίζει σε λέξεις.
@@ -17,21 +17,17 @@ def create_inverted_index():
         except TypeError:
             pass
 
-    for i in range(1240):
-        word_dict[i] = dict.fromkeys(total_words, 0)
-
-    for doc_number in range(1, 1240):  # 1239 έγγραφα
+    # Δημιουργία inverted index:
+    for doc_number in range(1, 100):  # 1239 έγγραφα
         doc = tools.get_doc(doc_number)
 
         try:
-            for doc_content in doc:  # Το doc βρίσκεται σε μορφή list, το doc_content το χωρίζει σε λέξεις.
-                for doc_word in doc_content.split():
-                    # Δημιουργία inverted index
+            for doc_content in doc:  # Το έγγραφο βρίσκεται σε μορφή list, το doc_content το χωρίζει σε λέξεις.
+                for doc_word in doc_content.split(): # κάθε λέξη του εγγράφου
                     if doc_word in inverted_index:
                         inverted_index[doc_word].add(doc_number)
                     else:
                         inverted_index[doc_word] = {doc_number}
-                    word_dict[doc_number][doc_word] += 1
 
             print(f"Διαβάζονται {doc_number}/1239 έγγραφα για τη δημιουργία του ανεστραμμένου ευρετηρίου")
         except TypeError:
@@ -40,4 +36,18 @@ def create_inverted_index():
     sorted_inverted_index = dict(sorted(inverted_index.items()))
     print("Το ανεστραμμένο ευρετήριο δημιουργήθηκε.")
 
-    return sorted_inverted_index, word_dict
+    return sorted_inverted_index, total_words
+
+
+def create_word_dict(total_words, inverted_index):
+    word_dict = {}
+
+    for i in range(1, 100):
+        word_dict[i] = dict.fromkeys(total_words, 0)
+
+    for i in range(1, 100):
+        for key, value in inverted_index.items():
+            for doc_number in value:
+                # print(f"BEFORE: word_dict[{doc_number}][{key}] = {word_dict[doc_number][key]}")
+                word_dict[doc_number][key] += 1
+                print(f"AFTER: word_dict[{doc_number}][{key}] = {word_dict[doc_number][key]}")
--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import vector_space_model
 
 
 def main_app():
-    sorted_inverted_index, word_dict = inverted_index.create_inverted_index()
-    docs_tf = vector_space_model.get_tf_dict(word_dict[1])  # dict[1] = πρώτο document
-    docs_idfs = vector_space_model.get_idf_dict(word_dict[1])
-    docs_tfidfs = vector_space_model.get_tfidf(docs_tf, docs_idfs)
+    sorted_inverted_index, total_words = inverted_index.create_inverted_index()
+    # print(sorted_inverted_index)
+    inverted_index.create_word_dict(total_words, sorted_inverted_index)
 
-    queries = tools.get_queries()
-    queries_dict = dict.fromkeys(queries, 1)
-    queries_tf = vector_space_model.get_tf_dict(queries_dict)
-    queries_idfs = vector_space_model.get_idf_dict(queries_dict)
+    # docs_tf = vector_space_model.get_tf_dict(word_dict[1])  # dict[1] = πρώτο document
+    # docs_idfs = vector_space_model.get_idf_dict(word_dict[1])
+    # docs_tfidfs = vector_space_model.get_tfidf(docs_tf, docs_idfs)
+
+    # queries = tools.get_queries()
+    # queries_dict = dict.fromkeys(queries, 1)
+    # queries_tf = vector_space_model.get_tf_dict(queries_dict)
+    # queries_idfs = vector_space_model.get_idf_dict(queries_dict)
 
     # Πρέπει να φτιαχτούν τα queries, να ολοκληρωθεί το vector space model και μετά (3), (4).
     # Ίσως χρειάζεται αναδιοργάνωση των συναρτήσεων από την αρχή για να ενσωματωθούν τα queries.
